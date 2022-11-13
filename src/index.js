c1fe80c959a7b1e29def055f5c7d5d3c65e09e9b
Julian Cardone <juliancardone1@gmail.com>
2022-11-13 17:07:09 -0500

    play/pause button inputted, select song working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-const { default: Example } = require("./scripts/example.js");
-const { default: AudioFetch } = require("./scripts/audio_fetch.js");
+const { default: PlaybackButton } = require("./scripts/play_button.js");
+const { default: FetchAudio } = require("./scripts/fetch_audio.js");
 
-document.addEventListener("DOMContentLoaded", () => {
-    const main = document.getElementById("main");
-    new Example(main);
-});
+document.addEventListener("DOMContentLoaded", ()=>{
+    const playing = document.getElementById("play/pause");
+    new PlaybackButton(playing);
+})
 
-document.addEventListener("DOMContentLoaded", () => {
-    const playback = document.getElementById("playback");
-    new AudioFetch(playback);
-});
+document.addEventListener("DOMContentLoaded", ()=>{
+    const instrumental = document.querySelector(".instrumentals");
+    new FetchAudio(instrumental);
+    // console.log(instrumental);
+})
---
+++ src/scripts/fetch_audio.js
@@ -0,0 +1,77 @@
+
+class FetchAudio {
+    constructor(ele){
+        this.ele = ele;
+
+        // for (let i = 1; i < this.ele.length; i++){
+        //     console.log(this.ele[i]);
+        //     this.ele.addEventListener("change", this.loadSong.bind(this.ele[i]))
+        // }
+        
+        this.ele.addEventListener("change", this.fetchSong.bind(this));
+
+    }
+
+    fetchSong(ele){
+
+        let song = ele.target.value;
+
+        let context = new AudioContext();
+
+        let audio = new Audio(`instrumentals/${song}.mp3`);
+
+        const source = context.createMediaElementSource(audio);
+        source.connect(context.destination);
+
+        audio.play();
+
+        // let audio;
+
+        // fetch(`instrumentals/${song}.mp3`)
+        //     .then(audioData => audioData.arrayBuffer())
+        //     .then(buffered => context.decodeAudioData(buffered))
+        //     .then(buffered => audio = buffered)
+
+        //     console.log(audio);
+        }
+    }
+    
+    // this.ele.children[0].innerText = "Ouch!";
+    
+
+export default FetchAudio;
+
+
+// class AudioFetch {
+
+//     constructor(ele){
+//         this.ele = ele;
+//         this.ele.innerHTML = "<h2>click to fetch and play</h2>"
+//         this.ele.addEventListener("click", this.fetchNPlay.bind(this));
+//     }
+
+//     fetchNPlay(){
+//         // const AudioContext = window.AudioContext || window.webkitAudioContext;
+//         const ctx = new AudioContext();
+
+//         let audio;
+
+//         fetch('https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3')
+//             .then(data => data.arrayBuffer())
+//             .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+//             .then(decodedAudio => {
+//                 audio = decodedAudio;
+//             });
+//             console.log(audio);
+//         //^^fetch aduio file, put into buffer, decode it tobe able
+//         //towork with it as anaudio node
+//         const playSound = ctx.createBufferSource();
+//         playSound.buffer = audio;
+//         playSound.connect(ctx.destination);
+//         playSound.start(ctx.currentTime);
+
+//         console.log(ctx);
+
+//         this.ele.innerHTML = "<h2>playing...</h2>"
+//     }
+// }
